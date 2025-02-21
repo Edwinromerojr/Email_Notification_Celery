8f7d01e3cf332994556d9e3181b30df99dcd859a
Edwin Romero <edwinrmrjr@gmail.com>
2025-02-21 11:26:04 +0800

    part3 monitor celery tasks with flower

--- a_core/settings.py
+++ a_core/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'a_home',
     'a_users',
     'messageboard',
+    'django_celery_results'
 ]
 
 SITE_ID = 1
@@ -160,3 +161,5 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_RESULT_EXTENDED = True
